6e18ac3e9c7ccef72f62945e2c69fa186ae8ed03
nojussta <nojus.stasiunas2@gmail.com>
2023-05-09 14:06:37 +0300

    add/location-class

--- ASA_IP/Location.cs
+++ ASA_IP/Location.cs
@@ -8,5 +8,24 @@ namespace ASA_IP
 {
     internal class Location
     {
+        public string Name { get; set; }
+        public long Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Location(string name, long id, double x, double y)
+        {
+            Name = name;
+            Id = id;
+            X = x;
+            Y = y;
+        }
+
+        public double DistanceTo(Location other)
+        {
+            double dx = X - other.X;
+            double dy = Y - other.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
     }
 }
