e3910dde2111cdaa2bc1110d3cf1bc6c887edd5f
nojussta <nojus.stasiunas2@gmail.com>
2023-05-17 12:27:46 +0300

    add/route-changes

--- ASA_IP/Route.cs
+++ ASA_IP/Route.cs
@@ -6,104 +6,35 @@ using System.Threading.Tasks;
 
 namespace ASA_IP
 {
-    internal class RoutePlanner
+    internal class Route
     {
-        public static List<Location> FindRoute(List<Location> locations)
+        public List<Location> VisitedLocations { get; set; }
+        public double Quality;
+
+        public Route()
         {
-            int numTravelers = 3;
-
-            // Calculate distances between all pairs of locations
-            double[,] distances = new double[locations.Count, locations.Count];
-            for (int i = 0; i < locations.Count; i++)
-            {
-                for (int j = 0; j < locations.Count; j++)
-                {
-                    distances[i, j] = locations[i].DistanceTo(locations[j]);
-                }
-            }
-
-            // Initialize variables for holding the best route and its cost
-            List<Location> bestRoute = null;
-            double bestCost = double.MaxValue;
-
-            // Generate all permutations of the locations
-            var permutations = GetPermutations(locations, numTravelers);
-
-            // Find the cheapest route among all permutations
-            foreach (var permutation in permutations)
-            {
-                double cost = 0;
-
-                // Calculate the cost of this route
-                for (int i = 0; i < permutation.Count - 1; i++)
-                {
-                    cost += Math.Sqrt(distances[permutation[i].Id, permutation[i + 1].Id]);
-                }
-
-                // If this is a cheaper route, update the best route and cost
-                if (cost < bestCost)
-                {
-                    bestRoute = permutation;
-                    bestCost = cost;
-                }
-            }
-
-            return bestRoute;
+            VisitedLocations = new List<Location>();
+            this.Quality = 0;
         }
 
-        public static List<List<Location>> GetPermutations<Location>(List<Location> list, int length)
-        {
-            var results = new List<List<Location>>();
-
-            if (length == 1)
-            {
-                foreach (var item in list)
-                {
-                    results.Add(new List<Location> { item });
-                }
-            }
-            else
-            {
-                foreach (var item in list)
-                {
-                    var remainingList = new List<Location>(list);
-                    remainingList.Remove(item);
-
-                    foreach (var permutation in GetPermutations(remainingList, length - 1))
-                    {
-                        permutation.Insert(0, item);
-                        results.Add(permutation);
-                    }
-                }
-            }
-
-            return results;
-        }
-
-        public static double CalculateRouteDistance(List<Location> locations)
+        public double GetTotalDistance()
         {
             double totalDistance = 0;
-            for (int i = 0; i < locations.Count - 1; i++)
+            for (int i = 0; i < VisitedLocations.Count - 1; i++)
             {
-                double distance = CalculateDistanceBetweenPoints(locations[i].X, locations[i].Y,
-                                                                  locations[i + 1].X, locations[i + 1].Y);
-                totalDistance += distance;
+                totalDistance += VisitedLocations[i].DistanceTo(VisitedLocations[i + 1]);
             }
-            // Add distance back to the starting location to complete the route
-            double finalDistance = CalculateDistanceBetweenPoints(locations[locations.Count - 1].X,
-                                                                   locations[locations.Count - 1].Y,
-                                                                   locations[0].X, locations[0].Y);
-            totalDistance += finalDistance;
             return totalDistance;
         }
 
-        public static double CalculateDistanceBetweenPoints(double x1, double y1, double x2, double y2)
+        public bool ContainsLocation(Location Location)
         {
-            double dx = x2 - x1;
-            double dy = y2 - y1;
-
-            return Math.Sqrt(dx * dx + dy * dy);
+            return VisitedLocations.Any(p => p.Id == Location.Id);
         }
 
+        public void AddLocation(Location Location)
+        {
+            VisitedLocations.Add(Location);
+        }
     }
 }
