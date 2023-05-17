efbfb624106a142baff10bafd8ac895b25c8d94c
nojussta <nojus.stasiunas2@gmail.com>
2023-05-17 12:26:59 +0300

    add/locationContainer

---
+++ ASA_IP/LocationContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_IP
+{
+    internal class LocationContainer
+    {
+        private List<Location> allLocations;
+
+        public LocationContainer()
+        {
+            allLocations = new List<Location>();
+        }
+
+        public void AddLocation(long id, string name, double x, double y)
+        {
+            allLocations.Add(new Location(id, name, x, y));
+        }
+
+        public void AddLocation(Location Location)
+        {
+            allLocations.Add(Location);
+        }
+
+        public List<Location> GetAllLocations()
+        {
+            return allLocations;
+        }
+    }
+}
--- ASA_IP/Program.cs
+++ ASA_IP/Program.cs
@@ -14,14 +14,14 @@ namespace ASA_IP
             List<Location> locations = InOutUtils.ReadData(fileName);
 
             //Console.WriteLine(RoutePlanner.FindRoute(locations));
-            var routes = RoutePlanner.GetPermutations(locations, locations.Count);
+            var routes = Route.GetPermutations(locations, locations.Count);
 
             var shortestRoute = new List<Location>();
             var shortestDistance = double.MaxValue;
 
             foreach (var route in routes)
             {
-                var distance = RoutePlanner.CalculateRouteDistance(route);
+                var distance = Route.CalculateRouteDistance(route);
 
                 if (distance < shortestDistance)
                 {
