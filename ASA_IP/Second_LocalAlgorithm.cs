f095ef664a2f1ffc28342ef2fe1abc5e454db635
nojussta <nojus.stasiunas2@gmail.com>
2023-05-18 23:27:31 +0300

    update/local-algo-complexity

--- ASA_IP/Second_LocalAlgorithm.cs
+++ ASA_IP/Second_LocalAlgorithm.cs
@@ -6,35 +6,36 @@ namespace ASA_IP
     {
         public static List<Location> Second_Local(double[,] adjMatrix, int start, List<Location> locations)
         {
-            int n = locations.Count;
-            bool[] visited = new bool[n];
-            List<Location> path = new List<Location> { locations[start] };
-            visited[start] = true;
+            int n = locations.Count;                                   // c | 1
+            bool[] visited = new bool[n];                               // c | n
+            List<Location> path = new List<Location> { locations[start] };   // c | 1
+            visited[start] = true;                                     // c | 1
 
-            while (path.Count < n)
+            while (path.Count < n)                                     // c | n
             {
-                int lastCityIndex = path.Count - 1;
-                double minDistance = double.MaxValue;
-                int closestCityIndex = -1;
+                int lastCityIndex = path.Count - 1;                     // c | n-1
+                double minDistance = double.MaxValue;                   // c | n-1
+                int closestCityIndex = -1;                              // c | n-1
 
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++)                             // c | n*(n-1)
                 {
-                    if (!visited[i] && adjMatrix[lastCityIndex, i] < minDistance)
+                    if (!visited[i] && adjMatrix[lastCityIndex, i] < minDistance)    // c | n*(n-1)
                     {
-                        minDistance = adjMatrix[lastCityIndex, i];
-                        closestCityIndex = i;
+                        minDistance = adjMatrix[lastCityIndex, i];      // c | n*(n-1)
+                        closestCityIndex = i;                           // c | n*(n-1)
                     }
                 }
 
-                if (closestCityIndex != -1)
+                if (closestCityIndex != -1)                             // c | n-1
                 {
-                    path.Add(locations[closestCityIndex]);
-                    visited[closestCityIndex] = true;
+                    path.Add(locations[closestCityIndex]);               // c | n-1
+                    visited[closestCityIndex] = true;                    // c | n-1
                 }
             }
 
-            path.Add(locations[start]);
-            return path;
+            path.Add(locations[start]);                                 // c | 1
+            return path;                                                // c | 1
         }
+
     }
 }
